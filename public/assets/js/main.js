2d454fe038d4515d3cfc7c9c1304f1afb2c2e326
pastrans <pastran2526@gmail.com>
2023-09-04 11:18:58 -0600

    eliminación de codigo de js, que esta generando error en la consola. arreglos de accesibilidad

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -165,12 +165,6 @@
     });
   }
 
-  /**
-   * Initiate  glightbox 
-   */
-  const glightbox = GLightbox({
-    selector: '.glightbox'
-  });
 
   /**
    * Skills animation
@@ -219,40 +213,6 @@
 
   });
 
-  /**
-   * Initiate portfolio lightbox 
-   */
-  const portfolioLightbox = GLightbox({
-    selector: '.portfolio-lightbox'
-  });
 
-  /**
-   * Portfolio details slider
-   */
-  new Swiper('.portfolio-details-slider', {
-    speed: 400,
-    loop: true,
-    autoplay: {
-      delay: 5000,
-      disableOnInteraction: false
-    },
-    pagination: {
-      el: '.swiper-pagination',
-      type: 'bullets',
-      clickable: true
-    }
-  });
-
-  /**
-   * Animation on scroll
-   */
-  window.addEventListener('load', () => {
-    AOS.init({
-      duration: 1000,
-      easing: "ease-in-out",
-      once: true,
-      mirror: false
-    });
-  });
 
 })()
